a54a9eef3a4d37bfb3976f79a151748a47b55573
Gustavo Alejandro Castellanos <gustavoaca1997@gmail.com>
2018-10-20 22:54:41 -0400

    Se agrega la ruta al estado. Se cambian las firmas. Se agrega la opción continuar ruta y preguntar ruta. Refactorización de las funciones de Sabio.

--- Sabio.hs
+++ Sabio.hs
@@ -4,47 +4,78 @@ import Control.Monad.Trans
 import Data.Maybe
 import qualified Control.Monad.State as St
  
-type LaberintoState = St.StateT Laberinto IO (Maybe Laberinto)
+type Ruta = [String]
+type LaberintoState = St.StateT (Laberinto, Ruta) IO ()
 
 {-Imprimir las opciones del usuario-}
-opciones :: IO ()
+opciones :: LaberintoState
 opciones = do
-    putStrLn "El sabio del laberinto"
-    putStrLn "Opciones:"
-    putStrLn "1: Comenzar a hablar de un laberinto nuevo"
-    putStrLn "2: Preguntar ruta"
-    putStrLn "3: Reportar pared abierta"
-    putStrLn "4: Reportar derrumbe"
-    putStrLn "5: Reportar tesoro tomado"
-    putStrLn "6: Reportar tesoro hallado"
-    putStrLn "7: Dar nombre al laberinto"
-    putStrLn "8: Hablar de un laberinto de nombre conocido"
-    putStrLn "9: Imprimir opciones"
-    putStrLn "10: Salir\n"
+    (curLab, curRuta) <- St.get -- obtenemos el estado actual
+
+    -- Imprimimos las opciones
+    lift $ putStrLn "El sabio del laberinto"
+    lift $ putStrLn "Opciones:"
+    lift $ putStrLn "1: Comenzar a hablar de un laberinto nuevo"
+
+    case curRuta of
+        [] -> lift $ putStrLn "2: Preguntar ruta" -- Si no hay ruta que seguir
+        _ -> do
+            lift $ putStrLn "2: Preguntar ruta nueva" -- Preguntar por nueva ruta
+            lift $ putStrLn "2.1: Continuar ruta" -- Continuar recorriendo ruta actual
+
+    lift $ putStrLn "3: Reportar pared abierta"
+    lift $ putStrLn "4: Reportar derrumbe"
+    lift $ putStrLn "5: Reportar tesoro tomado"
+    lift $ putStrLn "6: Reportar tesoro hallado"
+    lift $ putStrLn "7: Dar nombre al laberinto"
+    lift $ putStrLn "8: Hablar de un laberinto de nombre conocido"
+    lift $ putStrLn "9: Imprimir opciones"
+    lift $ putStrLn "10: Salir\n"
+    lift $ putStrLn "Ingrese opción:"
 
 {- Loop infinito para leer las opciones del usuario -}
 infi :: LaberintoState
 infi = do
+    (curLab, curRuta) <- St.get -- Obtenemos el estado actual
     opcion <- lift getLine -- leemos la opcion
     case opcion of
-        "1" -> do
-            laberintoNuevo
+        "1" -> do -- Comenzar a hablar de un laberinto nuevo
+            laberintoNuevo -- creamos nuevo laberinto
+            infi -- repetimos loop
+
+        "2" -> do -- Preguntar Ruta
+            lift $ putStrLn "Escribe la ruta separada por espacios. Ejemplo: derecha izquierda derecha recto."
+            rutaStr <- lift getLine -- leemos la ruta
+            let ruta = words rutaStr -- Obtenemos los caminos
+            St.put $ (curLab, ruta) -- Actualizamos la ruta actual
+            recorrerRuta
+            infi -- repetimos loop
+
+        "2.1" -> case curRuta of -- Continuar ruta
+            [] -> lift $ putStrLn "No hay ruta que seguir." -- Si no hay ruta que seguir
+            _ -> do
+                lift $ putStrLn "Escribe la ruta separada por espacios. Ejemplo: derecha izquierda derecha recto."
+                rutaStr <- lift getLine -- leemos la ruta
+                let ruta = words rutaStr -- Obtenemos los caminos
+                St.put $ (curLab, curRuta ++ ruta) -- Actualizamos la ruta actual
+                recorrerRuta
+                infi -- repetimos loop
+
+
+        "9" -> do -- Imprimir opciones
+            opciones
             infi
-        "9" -> do
-            lift opciones
-            infi
-        "10" -> do
+        "10" -> do -- Salir
             lift $ putStrLn "Chao viajero"
-            return Nothing
-        _ -> infi
+        _ -> error "Opción incorrecta"
         
 --------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-    opciones
-    St.runStateT infi laberintoDefault
+    St.runStateT opciones (laberintoDefault, [])
+    St.runStateT infi (laberintoDefault, [])
     return ()
 
 --------------------------------------------------------------------------------------------
@@ -56,8 +87,42 @@ laberintoNuevo = do
     lift $ putStrLn "Escribe la ruta separada por espacios. Ejemplo: derecha izquierda derecha recto."
     rutaStr <- lift getLine -- Leemos la ruta
 
-    -- Usamos el nuevo laberinto
-    let newLab = fromMaybe laberintoDefault (construirLaberinto (words rutaStr))
-    St.put $ newLab
-    lift $ putStrLn $ "Nuevo laberinto: " ++ show newLab
-    return Nothing
+    let newLab = fromMaybe laberintoDefault (construirLaberinto (words rutaStr)) -- obtenemos el nuevo laberinto
+    St.put $ (newLab, []) -- Lo escogemos como el nuevo laberinto
+
+    -- Imprimimos nuevo laberinto
+    -- lift $ putStrLn $ "Nuevo laberinto: " ++ show newLab
+
+{- Función que recorre el laberinto siguiendo una ruta -}
+recorrerRuta :: LaberintoState
+recorrerRuta = do
+    (curLab, curRuta) <- St.get -- Actual estado
+    let ret = recorrer (Just curLab) curRuta -- Recorremos el laberinto siguiendo la ruta
+    case ret of
+        Nothing -> do
+            lift $ putStrLn "No hay laberinto"
+            St.put (curLab, [])
+
+        -- laberintoDefault -> do -- Camino sin salida y no hay tesoro
+        --     lift $ putStrLn "Se ha llegado a un camino sin salida."
+        --     St.put (curLab, [])
+
+        Just lab -> case (tesoroLaberinto lab) of
+            Just tesoro -> do -- Hay tesoro
+                lift $ putStrLn $ "Se ha encontrado un tesoro: " ++ show tesoro
+                St.put (curLab, [])
+
+            Nothing -> -- No hay tesoro
+                case (trifurcacionLaberinto lab) of
+
+                    -- Camino sin salida
+                    Trifurcacion Nothing Nothing Nothing -> do
+                        lift $ putStrLn "Se ha llegado a un camino sin salida."
+                        St.put (curLab, [])
+
+                    -- Camino normal
+                    _ -> lift $ putStrLn $ "No se ha llegado ni a un camino sin salida ni a un tesoro. " ++
+                        "Para mostrar las opciones de nuevo, presiones 9."
+                
+
+        
